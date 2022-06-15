bffa44ac1423cb785621c78b2ef4852a82c33ba6
karlfroldan <karlfroldan@gmail.com>
2022-06-15 18:19:55 +0800

    add functor instance for segments

--- src/Data/Geometry/Segment.hs
+++ src/Data/Geometry/Segment.hs
@@ -3,3 +3,6 @@ module Data.Geometry.Segment where
 import Data.Geometry.Point
 
 data Segment2D a = Segment2D (Point2D a) (Point2D a) deriving (Eq, Read, Show)
+
+instance Functor Segment2D where 
+    fmap f (Segment2D p q) = Segment2D (f <$> p) (f <$> q)
