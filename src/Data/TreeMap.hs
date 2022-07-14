9c75c815c2908370136ae8018db3acf5259d421e
karlfroldan <karlfroldan@gmail.com>
2022-07-14 22:15:41 +0800

    add docs

--- src/Data/TreeMap.hs
+++ src/Data/TreeMap.hs
@@ -30,14 +30,24 @@ instance Foldable (TreeMap k) where
 -- instance (Show k, Show v) => Show (TreeMap k v) where 
 --     show t = BT.showTree (getTree t)
 
+-- | Create a TreeMap given a unary function `f` on keys and a list of key-value pairs.
+-- The function `f` itself does not get stored on the keys of the tree. Instead,
+-- it is used as a guiding value during insertion.
 fromListWith :: Ord k => (k -> k) -> [(k, v)] -> TreeMap k v 
 fromListWith f = TreeMap . BT.fromListWith f 
 
+-- | Create a TreeMap. The same as Data.Map.
 fromList :: Ord k => [(k, v)] -> TreeMap k v 
 fromList = fromListWith id
 
+-- | Insert into a tree given a unary function `f` which is used as a guide for tree traversal.
 insertWith :: Ord k => (k -> k) -> k -> v -> TreeMap k v -> TreeMap k v 
 insertWith f k v t = TreeMap (BT.insertWith f k v (getTree t))
+
+-- | Given a function `f` that acts as a guide for insertion traversal, we also 
+-- have a binary function `g` that modifies the corresponding value on the tree.
+update :: Ord k => (k -> k) -> (v -> v -> v) -> k -> v -> TreeMap k v -> TreeMap k v 
+update f g k v t = TreeMap (BT.insertWithUpdate f g k v (getTree t))
 
 insert :: Ord k => k -> v -> TreeMap k v -> TreeMap k v 
 insert = insertWith id 
